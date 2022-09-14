d2dcf5e99b6792c2074e77b2a13d66a41f072726
Voitech <wojciech.potiopa@gmail.com>
2022-09-14 09:42:31 +0200

    add paths to containers (#7)

--- model/src/main/scala/org/alax/model/container.scala
+++ model/src/main/scala/org/alax/model/container.scala
@@ -1,27 +1,29 @@
 package org.alax.model
 
-import org.alax.model.base.Statement;
+import org.alax.model.base.Statement
+
+import java.nio.file.Path;
 
 /**
  * Basically a file contains code
  *
  * @param statements
  */
-class CompilationUnit(val statements: Seq[Statement])
+class CompilationUnit(val statements: Seq[Statement], val path:Path)
 
 /**
  * Package of files containg code and other packages
  *
  * @param children
  */
-class Package(val children: Seq[CompilationUnit | Package])
+class Package(val children: Seq[CompilationUnit | Package],val path:Path)
 
 /**
  *
  * @param descriptor - Describes how module should interact with language services and other modules
  * @param children   - Packages contained in module, at least one package must be present.
  */
-class Module(val descriptor: Module.Descriptor, val children: Seq[Package])
+class Module(val descriptor: Module.Descriptor, val children: Seq[Package],val path:Path)
 
 object Module {
   /**
@@ -30,7 +32,7 @@ object Module {
   class Descriptor();
 }
 
-class Project(val descriptor: Project.Descriptor, val children: Seq[Module]);
+class Project(val descriptor: Project.Descriptor, val children: Seq[Module],val path:Path);
 
 object Project {
   /**
